4390563b1c3a8a993f12e5349177d3d2a26cd0bb
kristineaajuhl <kristineaajuhl@gmail.com>
2023-07-03 09:16:50 +0200

    Update transform_to_original_coordinates.py

--- transform_to_original_coordinates.py
+++ transform_to_original_coordinates.py
@@ -10,57 +10,76 @@ import os
 import numpy as np 
 import vtk 
 
-input_file = 'C:/Users/kajul/Desktop/Transform_debug/surf_cropped.vtk'
-norm_file = 'C:/Users/kajul/Desktop/Transform_debug/surf_cropped_transformed.vtk'
+#input_file = 'C:/Users/kajul/Desktop/Transform_debug/surf_cropped.vtk'
+#norm_file = 'C:/Users/kajul/Desktop/Transform_debug/surf_cropped_transformed.vtk'
+
+#vol_num = '15'
+#input_file = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v64_mShapeNet64Vox/evaluation_486_@256/generation/prepared_data/KIDNEY_HEALTHY_0016_SERIES0028_volume_'+vol_num+'/surf_cropped.vtk'
+#image_file = 'D:/DTUTeams/Kristine2023/kidney_ROI/ROI/img/KIDNEY_HEALTHY_0016_SERIES0028_volume_'+vol_num+'.nii'
+#output_file = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v64_mShapeNet64Vox/evaluation_486_@256/generation/prepared_data/KIDNEY_HEALTHY_0016_SERIES0028_volume_'+vol_num+'/surf_cropped_transformed.vtk'
+
+#input_file = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v64_mShapeNet64Vox/evaluation_486_@256/generation/prepared_data/CFA-PILOT_0008_SERIES00'+vol_num+'/surf_cropped.vtk'
+#image_file = 'D:/DTUTeams/Kristine2023/CFA_pilot/2mm_ROI/ROI/img/CFA-PILOT_0008_SERIES00'+vol_num+'.nii'
+#output_file = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v64_mShapeNet64Vox/evaluation_486_@256/generation/prepared_data/CFA-PILOT_0008_SERIES00'+vol_num+'/surf_cropped_transformed.vtk
 
 
-# ROI image
-img = sitk.ReadImage('C:/Users/kajul/Desktop/Transform_debug/KIDNEY_HEALTHY_0016_SERIES0028_volume_0.nii')
-size = img.GetSize()
-origin = img.GetOrigin()
-spacing = img.GetSpacing()
-direction = img.GetDirection()
-roi_physical_size = 70 #mm
+base = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v128_mShapeNet128Vox/evaluation_1142_@256/generation/prepared_data/'
+filelist = os.listdir(base)
 
 
-
-# Surface
-reader = vtk.vtkPolyDataReader()
-reader.SetFileName(input_file)
-reader.Update()
-surf = reader.GetOutput()  
+for file in filelist: 
+    input_file = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v128_mShapeNet128Vox/evaluation_1142_@256/generation/prepared_data/'+file+'/surf_cropped.vtk'
+    image_file = 'D:/DTUTeams/Kristine2023/CFA_BestPhase/ROI/img/'+file+'.nii'
+    output_file = 'D:/DTUTeams/Kristine2023/NUDF-main/experiments/iimage_dist-0.45_0.45_0.1_sigmas-in1_out1_0.0_v128_mShapeNet128Vox/evaluation_1142_@256/generation/prepared_data/'+file+'/surf_cropped_transformed.vtk'
 
 
-scale_factor = (roi_physical_size,roi_physical_size,roi_physical_size)
-t2 = vtk.vtkTransform()
-t2.Scale(scale_factor)
-
-scale_filter = vtk.vtkTransformFilter()
-scale_filter.SetInputData(surf)
-scale_filter.SetTransform(t2)
-scale_filter.Update()
-surf_scale = scale_filter.GetOutput()
-
-#Translate 
-#direction = np.array([img.GetDirection()[0],img.GetDirection()[4],img.GetDirection()[8]])
-#pd_direction = direction[::-1]
-#trans = (-direction - origin*scale_factor[0])*[-1,-1,1]#pd_direction
-trans = ((origin[0]+size[0]*spacing[0]/2),
-         (origin[1]-size[1]*spacing[1]/2),
-         (origin[2]+size[2]*spacing[2]/2))
-#trans = (46.86,15.14,-135.77)
-t1 = vtk.vtkTransform()
-t1.Translate(trans)
-
-print(trans)
-
-trans_filter = vtk.vtkTransformFilter()
-trans_filter.SetInputData(surf_scale)
-trans_filter.SetTransform(t1)
-trans_filter.Update()
-surf_transformed = trans_filter.GetOutput()
-
-writer = vtk.vtkPolyDataWriter()
-writer.SetFileName(norm_file)
-writer.SetInputData(surf_transformed)
-writer.Write()
+    # ROI image
+    img = sitk.ReadImage(image_file)
+    size = img.GetSize()
+    origin = img.GetOrigin()
+    spacing = img.GetSpacing()
+    direction = img.GetDirection()
+    roi_physical_size = 140 #mm
+    
+    
+    
+    # Surface
+    reader = vtk.vtkPolyDataReader()
+    reader.SetFileName(input_file)
+    reader.Update()
+    surf = reader.GetOutput()  
+    
+    
+    scale_factor = (-roi_physical_size/2,-roi_physical_size/2,roi_physical_size/2)
+    t2 = vtk.vtkTransform()
+    t2.Scale(scale_factor)
+    
+    scale_filter = vtk.vtkTransformFilter()
+    scale_filter.SetInputData(surf)
+    scale_filter.SetTransform(t2)
+    scale_filter.Update()
+    surf_scale = scale_filter.GetOutput()
+    
+    #Translate 
+    #direction = np.array([img.GetDirection()[0],img.GetDirection()[4],img.GetDirection()[8]])
+    #pd_direction = direction[::-1]
+    #trans = (-direction - origin*scale_factor[0])*[-1,-1,1]#pd_direction
+    trans = ((origin[0]+size[0]*spacing[0]/2),
+             (origin[1]-size[1]*spacing[1]/2),
+             (origin[2]+size[2]*spacing[2]/2))
+    #trans = (46.86,15.14,-135.77)
+    t1 = vtk.vtkTransform()
+    t1.Translate(trans)
+    
+    print(trans)
+    
+    trans_filter = vtk.vtkTransformFilter()
+    trans_filter.SetInputData(surf_scale)
+    trans_filter.SetTransform(t1)
+    trans_filter.Update()
+    surf_transformed = trans_filter.GetOutput()
+    
+    writer = vtk.vtkPolyDataWriter()
+    writer.SetFileName(output_file)
+    writer.SetInputData(surf_transformed)
+    writer.Write()
